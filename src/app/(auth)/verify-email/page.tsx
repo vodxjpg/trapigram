34091409192f1ae7aa6e4248d3d093cb92494e41
Vodx <vodx@proton.me>
2025-06-16 13:42:48 +0200

    changes to logo

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -10,7 +10,6 @@ export default function VerifyEmailPage() {
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
             </div>
             Trapyfy.
           </a>
