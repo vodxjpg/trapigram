f66d70eaeb095d30160805b69541470e9a7ad9b6
Vodx <vodx@proton.me>
2025-07-08 20:03:52 +0200

    changes to ui

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -8,8 +8,8 @@ interface PageHeaderProps {
 
 export function PageHeader({ title, description, actions }: PageHeaderProps) {
   return (
-    <div className="flex items-center justify-between">
-      <div>
+    <div className="flex items-center justify-between flex-col md:flex-row">
+      <div className="mr-auto mb-2">
         <h1 className="text-2xl font-bold tracking-tight">{title}</h1>
         {description && <p className="text-muted-foreground">{description}</p>}
       </div>
